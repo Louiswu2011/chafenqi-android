2cf2bc4ec08bbad5921cd7756402071c6fa9fdad
Louis Wu <louiswu2011@126.com>
2024-06-06 13:38:14 +0900

    bump dependency version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -160,7 +160,7 @@ dependencies {
     implementation("androidx.core:core-ktx:1.13.1")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx")
     implementation("androidx.activity:activity-compose")
-    implementation("com.google.firebase:firebase-analytics:22.0.0")
+    implementation("com.google.firebase:firebase-analytics:22.0.1")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
